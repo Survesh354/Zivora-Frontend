ad52367f7ad2edff8401f2f67e4735dc228a54fa
survesh354@gmail.com <survesh354@gmail.com>
2026-03-02 01:21:25 +0530

    case change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Taskbar from "./components/taskbar";
 import Home from "./components/Home";
 import About from "./components/about";
 import Cart from "./components/cart";
-import Wishlist from "./components/Wishlist";
+import Wishlist from "./components/wishlist";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Shop from "./components/Shop";
